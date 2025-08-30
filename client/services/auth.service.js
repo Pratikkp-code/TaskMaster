4dbb3173fc83bd63d674202b4981db2d7b0322df
Pratik Kapure <pratikkapure20@gmail.com>
2025-08-30 18:19:36 +0530

    Updated1 auth.service.js

--- client/services/auth.service.js
+++ client/services/auth.service.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
 
-const API_URL = 'http://localhost:4001/api/auth/';
+const API_HOST = process.env.NEXT_PUBLIC_AUTH_API_URL 
+  ? `https://${process.env.NEXT_PUBLIC_AUTH_API_URL}` 
+  : 'http://localhost:4001';
+const API_URL = `${API_HOST}/api/auth/`;
 
 const register = (name, email, password) => {
   return axios.post(API_URL + 'register', {
