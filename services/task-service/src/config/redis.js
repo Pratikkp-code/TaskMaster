e4e5972923089a33ecb8b15429cde67b21b642b9
Pratik Kapure <pratikkapure20@gmail.com>
2025-08-30 11:25:34 +0530

    Update redis.js

--- services/task-service/src/config/redis.js
+++ services/task-service/src/config/redis.js
@@ -1,7 +1,7 @@
 import { createClient } from 'redis';
 
 const redisClient = createClient({
-  url: `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`
+  url: process.env.REDIS_URL || `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`
 });
 
 redisClient.on('error', (err) => console.log('Redis Client Error', err));
