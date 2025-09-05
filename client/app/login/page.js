2b30d045678cc2f290fe9da613f6ca24e4a62ac0
Pratik Kapure <pratikkapure20@gmail.com>
2025-09-05 19:23:01 +0530

    Update1 login

--- client/app/login/page.js
+++ client/app/login/page.js
@@ -4,6 +4,7 @@ import { useState } from 'react';
 import { useRouter } from 'next/navigation';
 import Link from 'next/link'; 
 import authService from '../../services/auth.service.js';
+import { motion } from 'framer-motion';
 
 export default function Login() {
   const [email, setEmail] = useState('');
